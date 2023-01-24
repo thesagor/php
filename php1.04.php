586ea3cc9fe5ab279738df9db29adb1e5c0553a8
Sagor Ahmed <nilsagor004@gmail.com>
2023-01-24 21:33:45 +0600

    printf is just awsome to work and knowing many things like array boolean and many things

---
+++ php1.04.php
@@ -0,0 +1,17 @@
+<?php
+$ami= 'sagor';
+$pi=3;
+$result=true;
+var_dump($result);
+
+/*
+1. Integer
+2. boolen
+3. float
+4. array
+5.null
+*/
+ //echo "$result";
+// work with printf
+
+printf(" %s the student of class %s. He can't obtained number %s in his life", strtoupper($ami), $pi, $result);
