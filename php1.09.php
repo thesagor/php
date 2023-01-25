3bc47c40870aa5bbd1a2206608fc5dfa418e72a6
Sagor Ahmed <nilsagor004@gmail.com>
2023-01-25 19:17:09 +0600

    nuumver system learned nice part

---
+++ php1.09.php
@@ -0,0 +1,6 @@
+<?php
+// numvber system convert
+$d= 500;
+printf("Number convert decimel number %d to octal %o \n", $d, $d,);
+printf("Number convert decimel number %d to binary %b \n", $d, $d);
+printf("Number convert decimel number %d to hexadecimel %x \n", $d, $d);
