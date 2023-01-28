9d75bd8a6646acb878d6ff04db75ee76ba5d14ec
Sagor Ahmed <nilsagor004@gmail.com>
2023-01-28 08:36:57 +0600

    Learn more about arrithmetic and printf option

---
+++ php1.10.php
@@ -0,0 +1,16 @@
+<?php
+// numvber system convert
+$num1=34;
+$num2=23.55675;
+$num3= 14;
+
+$n=12.985554;
+$pn=25586;
+$mail= "xcode@gmail.com";
+$name= "Sagor";
+
+//printf('we ar using name %3$s mail %2$s phone number %1$d', $pn, $mail, $name);
+//printf("This is showing two floating number is %.2f", $n);
+//printf('Bianry of %1$d is %1$b', 1253);
+//printf("Samation of num1= %04d and number 3 is %04d", $num1,$num3);
+//printf("This is for all is %06.4f", $num2);
