216bf45475efc7859d4f9ef617cb16205935cd79
Sagor Ahmed <nilsagor004@gmail.com>
2023-01-30 07:53:28 +0600

    checking && and || very interesting

--- php1.12.php
+++ php1.12.php
@@ -1,18 +1,9 @@
 <?php
 // Logical operator 
-$n=18;
+$cow="jersey";
 
-// Chekc tennerger age or nor
-if($n>=18 && $n<=19) {
-    echo " This is teenger sathe sathe adult o ";
+if("friesian"== $cow || "jersey"==$cow){
+    echo " This is onekind of goru";
+}else{
+    echo "Amr kono gaan nai ei bisoye";
 }
-elseif($n>=13 && $n<= 19) {
-    echo " This This teenger age";
-}
-elseif($n>=18){
-    echo " This is not teeneger but adult";
-}
-else if ($n<=12) {
-    echo " This is child so that we should care them";
-}
-else{ echo "I have no knowledge about it";}
