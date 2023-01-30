74f8ea9dddec475efeff53c9c2e71f23b64c886d
Sagor Ahmed <nilsagor004@gmail.com>
2023-01-30 07:46:22 +0600

    Learn about ==,!=,>=,<=,else if , more

--- php1.11.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Return output showing
-$fname= "Mohammad Ahmad";
-$lname= "akash";
-$outputcai= sprintf("His name is %s %s",$fname, $lname);
-echo mb_strtoupper($outputcai);
---
+++ php1.12.php
@@ -0,0 +1,18 @@
+<?php
+// Logical operator 
+$n=18;
+
+// Chekc tennerger age or nor
+if($n>=18 && $n<=19) {
+    echo " This is teenger sathe sathe adult o ";
+}
+elseif($n>=13 && $n<= 19) {
+    echo " This This teenger age";
+}
+elseif($n>=18){
+    echo " This is not teeneger but adult";
+}
+else if ($n<=12) {
+    echo " This is child so that we should care them";
+}
+else{ echo "I have no knowledge about it";}
