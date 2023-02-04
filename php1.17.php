415a32c2c874ddf0612a4ea38af684d2a2242a1a
Sagor Ahmed <nilsagor004@gmail.com>
2023-02-04 07:36:54 +0600

    SWITCH case using in this tutorial

--- php1.17.php
+++ php1.17.php
@@ -1,7 +1,7 @@
 <?php
 // Swirch case diye even odd negetive and possitive checking
 
-$n= -21;
+$n= 24;
 $r= $n%2;
 
 switch($r){
@@ -14,6 +14,7 @@ switch($r){
             echo "This is negetive even number";
             break;
     }
+    break;
 
         default:switch($n){
             case ($n>0): 
@@ -24,8 +25,27 @@ switch($r){
                 echo "This is negetive odd number";
                 break;
         }
+        break;
 }
 
+echo "\n";
+
+switch (true){
+    case (0 == $r && $n>0):
+            echo " This is possitive and even number";
+            break;
+    case(1==$r && $n>0):
+            echo " This is ODDy and Possitive Number";
+            break;
+    case(0==$r && $n<0):
+            echo " This is Even number but Negtive";
+            break;
+        
+    case(-1 ==$r && $n<0):
+            echo "This is ODD number but negetive";
+            break;
+        
+    }
 
 
 
