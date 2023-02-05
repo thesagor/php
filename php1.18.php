9d9bdd427e252f6a5588dff19bf4839a8f1e4568
Sagor Ahmed <nilsagor004@gmail.com>
2023-02-05 07:20:49 +0600

    In this class halka detials about switch case learned

---
+++ php1.18.php
@@ -0,0 +1,13 @@
+<?php
+$name= "1Sagor Ahmed";
+switch($name){
+    case "1Sagor":
+        echo "This is sagor ahmed";
+        break;
+    case "1Sagor Ahmed": 
+        echo " This is original sagor";
+        break;
+    case "1sagor ahmed": 
+        echo "This is normalize sagor ahmed";
+        break;
+}
