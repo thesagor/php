0e3199206c0a4993fbee599ec8283e27251eceec
Sagor Ahmed <nilsagor004@gmail.com>
2023-02-14 07:22:59 +0600

    Another way to find factorial number

--- php1.24.php
+++ php1.24.php
@@ -1,6 +1,7 @@
 <?php
-$n=5;
-for($i=$n, $fact=1; $i>1; $i--){
-   $fact*= $i;
-}
-printf("Tis is %d is %d", $n, $fact);
+   $num=5;
+   for($i=1,$fac=1; $i<=$num;$i++){
+      $fac=$fac*$i;
+   }
+   
+   printf("This is the main number is %d and factorial %d", $num,$fac);
