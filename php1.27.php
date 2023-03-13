7ab884770dab47a1835edd3c55be6cd0413eca5d
Sagor Ahmed <nilsagor004@gmail.com>
2023-03-13 07:04:09 +0600

    while loop and ++ operator working

--- php1.27.php
+++ php1.27.php
@@ -1,5 +1,29 @@
 <?php
 
-   for($i=0; $i<100; $i++){
-      echo $i%10==0 ? $i : '';
+  //while loop and ++ operator
+   $i=0;
+   while($i<5){
+      echo $i;
+      echo PHP_EOL;
+      $i++;
    }
+
+   echo "===========\n";
+
+   $j=0;
+   while($j++ <5){
+      echo $j;
+      echo PHP_EOL;
+   }
+
+   echo "===========\n";
+
+   $n=0;
+   while(++$n <5){
+      echo $n;
+      echo PHP_EOL;
+   }
+
+   $x=$y=5;
+   $x=$y++;
+   echo "x is $x and y is $y";
