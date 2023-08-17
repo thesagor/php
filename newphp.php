146db83cb2ce6743f0f6632dfdd262a33ca961a1
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-17 11:01:03 +0600

    Result or grade find

--- newphp.php
+++ newphp.php
@@ -1,9 +1,12 @@
 //task of darun it
 <?php
-$student=52;
+$student=-25;
     $marks=$student;
 
-    if($marks==33 && $marks<=39){
+    if($marks>=0 && $marks<=32){
+        echo "Sala fail korese. Mor";
+    }
+    elseif($marks>=33 && $marks<=39){
         echo " The result is Grade Pass";
     }elseif($marks>=39 && $marks<=49){
         echo " The result is Grade D";
@@ -16,5 +19,5 @@ $student=52;
     }elseif($marks>=79 && $marks<=100){
         echo " The result is Grade A+";
     }else{
-        echo " Sala fail korese. Mor";
+        echo " Invalid number";
     }
