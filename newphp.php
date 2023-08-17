a649f1701c91500f45837e048b522028abe780f8
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-17 10:14:12 +0600

    comparison operator

--- newphp.php
+++ newphp.php
@@ -1,12 +1,10 @@
 //task of darun it
 <?php
-    $first = 10;
-    $sec   = 15;
-    $third = 20;
-    if($first>$sec){
-        echo "The highest number is First= $first" ;
-    }elseif($sec>$third){
-        echo "Second i sthe highest numbyer $second";
+    $a=25;
+    $b='25';
+    if($a===$b){
+        echo " Both are same";
     }else{
-        echo " Third is the largest number";
+        echo " They are not same";
+        //var_dump($b);
     }
