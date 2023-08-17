6697eb03bff0e9efa60f3aabeda5dce2c4250e3f
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-17 11:26:39 +0600

    finding the odd number

--- newphp.php
+++ newphp.php
@@ -1,6 +1,10 @@
 //task of darun it
 <?php
 $a=0;
-for ($i=0; $i <=100; $i++) { 
-    echo "$i\n";
+//find out the odd number
+
+for ($i=0; $i <=1000 ; $i++) { 
+    if($i%2==0){
+        echo "$i \n";
+    }
 }
