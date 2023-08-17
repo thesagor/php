a6e845ddaf0e70acd97c6e5af5604bf42590d23c
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-17 10:35:46 +0600

    Another commit

--- newphp.php
+++ newphp.php
@@ -2,17 +2,11 @@
 <?php
     $a=10;
     $b=8;
-    $c=5;
-    if($a>$b){
-        if($a>$c){
-            echo "A is greater number";
-        }elseif($b>$a){
-            if($b>$c){
-                echo " B is the greater number";
-            }
-        }
-        
-        else{
-            echo "C is the greater number";
-        }
+    $c=25;
+    if($a>$b && $a>$c){
+        echo " A is the greater number";
+    }elseif ($b>$a && $b>$c ){
+        echo "B is the greater number";
+    }else{
+        echo "C is the greater number";
     }
