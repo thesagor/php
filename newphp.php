1bdffaeab49da93cb9ee95060e3c43379355e97f
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-17 11:18:42 +0600

    switch case

--- newphp.php
+++ newphp.php
@@ -1,6 +1,6 @@
 //task of darun it
 <?php
-$student=-25;
+$student=00;
     $marks=$student;
 
     if($marks>=0 && $marks<=32){
@@ -21,3 +21,45 @@ $student=-25;
     }else{
         echo " Invalid number";
     }
+
+//switch case added
+
+$marks=00;
+switch ($marks) {
+    case ($marks>=0 && $marks<=32):
+        echo "This is fail";
+        break;
+        
+        case ($marks>=32 && $marks<=39):
+        echo "This is grade Pass Number";
+        break;
+    
+        
+        case ($marks>=39 && $marks<=49):
+        echo "This is grade D";
+        break;
+    
+        
+        case ($marks>=49 && $marks<=59):
+        echo "This is Grade C";
+        break;
+    
+        
+        case ($marks>=59 && $marks<=69):
+        echo "This is Grade B";
+        break;
+    
+        
+        case ($marks>=69 && $marks<=79):
+        echo "This is Grade A";
+        break;
+    
+        
+        case ($marks>=79 && $marks<=100):
+        echo "This is A++";
+        break;
+    
+    default:
+        echo " This is invalid number";
+        break;
+}
