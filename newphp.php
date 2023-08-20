24fd43824d8c684db9d94b06d645f2243cda9e01
Sagor Ahmed <nilsagor004@gmail.com>
2023-08-20 21:46:31 +0600

    find the even number with do while loop

--- newphp.php
+++ newphp.php
@@ -1,20 +1,11 @@
 <?php
 //Darun iT PHP course
-//find out the odd number
-/* for ($i=10; $i <=1000 ; $i++) { 
-    if($i%2 !=0){
-        echo "$i \n";
-    }
-}
-*/
+// find out the even number with do while loop
 
-/* $a=10;
-while ($a <= 1000) {
-    echo "$a\n";
-    $a++;
-} */
-$a=1;
+$a=0;
 do{
-    echo "$a\n";
+    if($a%2==0){
+        echo "$a\n";
+    }
     $a++;
-}while($a<=100);
+}while($a<=1000);
