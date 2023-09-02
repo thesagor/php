363a7d416de3beb8b52f42dbbf02d2e4c483452a
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-02 08:27:38 +0600

    assosiative array

--- func.php
+++ func.php
@@ -1,12 +1,9 @@
 <?php
-	//Array pop
-	 $arr = array("red","green","blue","yellow");							
+	//assoiative array
+	$students=[
+		'name'=>'John',
+		'13'=>'Mary',
+		'ju'=>'Jane'
+	];
 
-	array_pop($arr); //remove last element
-	array_push($arr,"Sagor");//add element to the end of the array
-	array_shift($arr); //remove first element	
-	 array_unshift($arr,"Modu"); //add element to the beginning of the array	
-
-
-	 $arr[1]="Dsasrk glue";
-	 print_r($arr);
+	echo $students['ju'];	//John
