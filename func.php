0a16ec87c1e04ac4c095814a786a68175f8c49cb
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-02 08:51:20 +0600

    Foreach and manyy types loops run on assosiative arrays

--- func.php
+++ func.php
@@ -1,9 +1,20 @@
 <?php
 	//assoiative array
-	$students=[
-		'name'=>'John',
-		'13'=>'Mary',
-		'ju'=>'Jane'
+	$schools=[
+		"class 9"=>"St. Joseph, ideal, biggan, motijheel",
+		"class 10"=>"begum badrunnesa, hazrat shah ali mohila, motijheel",
+		"class 8"=>"hamdard,high school,taight school, motijheel",
+		"university"=> "Du, Ru, Buet, Iut, Nsu, Aiub, Brac, East West,"
 	];
 
-	echo $students['ju'];	//John
+	// foreach ($schools as $vlo => $value) {
+		
+	// 	echo $vlo.": ".$value."\n";
+		
+	// }
+
+	$value=array_values($schools);
+	for($i=0;$i<=count($value);$i++){
+		echo $value[$i];
+		echo "\n";
+	}
