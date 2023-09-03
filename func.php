c65a4c6e62c3bd52448730028788dbf1d5c8c172
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-03 10:33:22 +0600

    Function of check odd and sum in reduce value

--- func.php
+++ func.php
@@ -2,10 +2,14 @@
 	//Reduce array to a single value by using a callback function
 	$mynum=[1,2,3,4,5,6,7,8,9,10];
 	function sum($oldvalue=0,$newvalue){
-		return $oldvalue+$newvalue;
+		if($newvalue%2==0){
+			return $oldvalue+$newvalue;
+
+		}
+		return $oldvalue;
 	}
 
-	$output= array_reduce($mynum,"sum");
+	$output= array_reduce($mynum, "sum");
 	echo $output;
 
 	
