bf6a877a8552d215483c95a34ca9d4bdcc8379df
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-04 19:06:21 +0600

    Null coalesing operator

--- func.php
+++ func.php
@@ -1,17 +1,12 @@
+
+
+<form action="" method="get">
+    <input type="text" name="user">
+    <input type="submit" value="Submit">
+</form>
+
 <?php
-	//Reduce array to a single value by using a callback function
-	$mynum=[1,2,3,4,5,6,7,8,9,10];
-	function sum($oldvalue=0,$newvalue){
-		if($newvalue%2==0){
-			return $oldvalue+$newvalue;
+//Null coalescing operator
+$user = $_GET['user'] ?? 'nobody';
 
-		}
-		return $oldvalue;
-	}
-
-	$output= array_reduce($mynum, "sum");
-	echo $output;
-
-	
-
-
+echo $user;
