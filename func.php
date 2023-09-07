a6d10dd8a1b2fd7f99325784a6c7b223c7e8d447
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-07 15:40:27 +0600

    remove a value and key from assosiative array

--- func.php
+++ func.php
@@ -7,21 +7,8 @@ $names= [
     "Age"=> 25
 ];
 
-$data= $names;
-
-print_r($data);
-
-echo "Shallow copy start from here \n";
-
-//copy by refarence or shallow copy
-
-function DaTa(&$data){
-
-    $data['name'] = 'Lal Hossain';
-    print_r($data);
-
-}
-Data($data);
-print_r($data);
+unset($names['name']);
+print_r($names);
+print_r( array_keys($names));
 
 
