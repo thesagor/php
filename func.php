559367c9cc54edc064a3e283201dac2b5cac1c2e
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-10 08:59:50 +0600

    usin null in array

--- func.php
+++ func.php
@@ -1,17 +1,15 @@
 <?php  
 //Array merge()
-$vegetable= ['Data', 'shak','pui', 'alu'];
+$vegetable= ['Data', 'shak','pui', 'alu','kaca','sukna moris', 'peyaj'];
 $fruits= ['Mango', 'Bedena', 'Kola', 'Jackfruits'];
 
-$part1=array_splice($vegetable,0,2);
+$part1=array_slice($vegetable,0,3,true);
+$part2=array_slice($vegetable,3,null,true);
 print_r($part1);
-print_r($vegetable);
-echo "\n";
-$mergedData= array_merge($part1, $vegetable);
-print_r($mergedData);
+print_r($part2);
 
 
 
 
-
-
+$preserved= $part1+ $part2;
+print_r($preserved);
