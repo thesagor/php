265578a71c2cb8d95bdefa4c21ec81a543cb7ee1
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-10 12:37:36 +0600

    seacrch from array

--- func.php
+++ func.php
@@ -5,11 +5,11 @@ $abol=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'Ja
 $numbers= [12,33,2,234,34,32,1,11,33,22,555,3,4,5];
 $knumber= ['a'=>10, 'd'=> 15, 'c'=>12, 'f'=>11];
 
+//find a value from array
+$offset= key_exists('jackfruit', $abol);
 
-asort($abol, SORT_STRING|SORT_FLAG_CASE);
-
-foreach($abol as $fruits){
-    echo $fruits. "\n";
+if(key_exists('c', $knumber)){
+    echo " Key has been exists";
 }
 
 
