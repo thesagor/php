20a98a8626cfa131c7126e4f83f92203c02ef1c9
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-10 15:07:54 +0600

    common elements from array

--- func.php
+++ func.php
@@ -3,9 +3,12 @@
 
 $fruits=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'Jadu' ,"ciege","Banana"];
 $numbers= [12,33,2,234,34,32,1,11,33,22,555,3,4,5];
-$knumber= ['a'=>10, 'd'=> 15, 'c'=>12, 'f'=>11];
+$number1= ['a'=>10, 'b'=> 15, 'c'=>122, 'd'=>113];
+$number2= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
 $desifruits= ["Mango", "Lemon", "Banana", "Jackfruits", "JADU"];
 
-$common= array_intersect($desifruits,$fruits);
+
+
+$common= array_diff_assoc($number1, $number2);
 print_r($common);
 
