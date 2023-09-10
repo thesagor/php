b332771e33f933732c9d5ae892b741ee52b0b38e
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-10 14:38:26 +0600

    array_intersect

--- func.php
+++ func.php
@@ -1,16 +1,11 @@
 <?php  
-//type of sort function
+//Common elements identify from the array
 
-$abol=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'Jadu' ,"ciege"];
+$fruits=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'Jadu' ,"ciege","Banana"];
 $numbers= [12,33,2,234,34,32,1,11,33,22,555,3,4,5];
 $knumber= ['a'=>10, 'd'=> 15, 'c'=>12, 'f'=>11];
+$desifruits= ["Mango", "Lemon", "Banana", "Jackfruits", "JADU"];
 
-//find a value from array
-$offset= key_exists('jackfruit', $abol);
-
-if(key_exists('c', $knumber)){
-    echo " Key has been exists";
-}
-
-
+$common= array_intersect($desifruits,$fruits);
+print_r($common);
 
