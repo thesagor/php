a952bf15a01a3ad15a6dbce329cefd1c12bcd33d
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-11 09:54:42 +0600

    Array filter is just osthir

--- func.php
+++ func.php
@@ -5,15 +5,20 @@ $fruits=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'
 $numbers= [12,33,2,234,34,32,1,11,33,22,555,3,4,5];
 $number1= ['a'=>10, 'b'=> 15, 'c'=>122, 'd'=>113];
 $number2= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
-$desifruits= ["Mango", "Lemon", "Banana", "Jackfruits", "JADU"];
+$desifruits= ["mango", "Lemon", "Banana", "Jackfruits", "JADU","manno", "manus", "Mama"];
 
 $num= [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
-function square($n){
-    return $n*$n*$n;
+
+function mcheck($m){
+    return $m[0]=="m";
 }
 
-$myarr= array_map('square',$num);
-print_r($myarr);
-print_r($num);
+function odd($n){
+    return $n%2==1;
+}
 
+$mchecked= array_filter($desifruits,'mcheck');
+//$oddcheck = array_filter($num,'odd');
+print_r($mchecked);
+//print_r($oddcheck);
