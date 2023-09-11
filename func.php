75d119c81449cba4a64ca9eb0d560e6d1ae40a6c
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-11 10:16:20 +0600

    Array reduce

--- func.php
+++ func.php
@@ -7,18 +7,14 @@ $number1= ['a'=>10, 'b'=> 15, 'c'=>122, 'd'=>113];
 $number2= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
 $desifruits= ["mango", "Lemon", "Banana", "Jackfruits", "JADU","manno", "manus", "Mama"];
 
-$num= [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+$num= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-
-function mcheck($m){
-    return $m[0]=="m";
+function jogfoll($oldvalue=0, $newvalue){
+    if($newvalue%2==0){
+        return $oldvalue+$newvalue;
+    }
+    return $oldvalue;
 }
 
-function odd($n){
-    return $n%2==1;
-}
-
-$mchecked= array_filter($desifruits,'mcheck');
-//$oddcheck = array_filter($num,'odd');
-print_r($mchecked);
-//print_r($oddcheck);
+$myjog = array_reduce($num,'jogfoll',4);
+echo "Jog foll of jor songkha $myjog";
