6bf5d7c66409f8b6e5d0bb54664a097af9c2562a
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-11 10:42:10 +0600

    list function

--- func.php
+++ func.php
@@ -7,14 +7,9 @@ $number1= ['a'=>10, 'b'=> 15, 'c'=>122, 'd'=>113];
 $number2= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
 $desifruits= ["mango", "Lemon", "Banana", "Jackfruits", "JADU","manno", "manus", "Mama"];
 
-$num= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+//list function of array
 
-function jogfoll($oldvalue=0, $newvalue){
-    if($newvalue%2==0){
-        return $oldvalue+$newvalue;
-    }
-    return $oldvalue;
-}
+$field=['fname','lname','email', 'meessage'];
 
-$myjog = array_reduce($num,'jogfoll',4);
-echo "Jog foll of jor songkha $myjog";
+list($fn, $ln, $mail, $msn) = ['fname','lname','email', 'meessage'];
+echo $fn;
