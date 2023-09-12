fc477c537125e09bf197a9e3755f45a8835cde95
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-12 21:48:20 +0600

    array_rand with shuffle

--- func.php
+++ func.php
@@ -4,19 +4,19 @@
 $fruits=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'Jadu' ,"ciege","Banana"];
 $numbers= [12,33,2,234,34,32,1,11,33,22,555,3,4,5];
 $number1= ['a'=>10, 'b'=> 15, 'c'=>122, 'd'=>113];
-$number2= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
+$num= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
 $desifruits= ["mango", "Lemon", "Banana", "Jackfruits", "JADU","manno", "manus", "Mama"];
 
-//list function of array
- $n= range(01,50);
-$rn=mt_rand(5,10);
-$mynum= $n[$rn];
+//suffle array
 
-if($mynum%2==0){
-    echo "Head";
-}else{
-    echo "Tail";
-}
+//$mys= shuffle($num);
+//print_r($num);
+
+$myrand= array_rand($num);
+print_r($myrand);
+
+
+
 
 
 
