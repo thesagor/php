88fe7c58b07048dbf9854931cf7bad16d43145af
Sagor Ahmed <nilsagor004@gmail.com>
2023-09-14 02:12:54 +0600

    module 4

--- func.php
+++ func.php
@@ -1,19 +1,8 @@
 <?php  
-//Common elements identify from the array
+//webdevelopment with php has been started
+//Module 4
 
-$fruits=["Apple", "Bannana","Cadbium","jackfruit","pineapple","abogoda","JADU",'Jadu' ,"ciege","Banana"];
-$numbers= [12,33,2,234,34,32,1,11,33,22,555,3,4,5];
-$number1= ['a'=>10, 'b'=> 15, 'c'=>122, 'd'=>113];
-$num= ['d'=>110, 'g'=> 125, 'd'=>113, 'f'=>11];
-$desifruits= ["mango", "Lemon", "Banana", "Jackfruits", "JADU","manno", "manus", "Mama"];
 
-//suffle array
-
-//$mys= shuffle($num);
-//print_r($num);
-
-$myrand= array_rand($num);
-print_r($myrand);
 
 
 
