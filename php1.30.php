87437f3f6555804dc4da78d2c18905ad9ff09567
Sagor Ahmed <nilsagor004@gmail.com>
2024-01-06 19:31:50 +0600

    function has been started

--- php1.30.php
+++ php1.30.php
@@ -1,13 +1,14 @@
 <?php
+//work with function
 
-$vold=0;
-$old=1;
-$n=1;
-for ($i=0; $i<10; $i++){
-    echo $vold." ";
-    $n=$vold+$old;
-    $vold=$old;
-    $old=$n;
+function factorial($n){
+    $fac=1;
+    for($i=$n; $i>0; $i--){
+        $fac*=$i;
+    }
+    return $fac;
 }
+echo factorial(6);
+
 
 
