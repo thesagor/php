8d2a8e1ec1b03409a69b195d3a44dc75ca2ba00c
Sagor Ahmed <nilsagor004@gmail.com>
2024-01-07 07:55:28 +0600

    taking unlimited numbers

--- func.php
+++ func.php
@@ -1,12 +1,33 @@
-<?php  
-//webdevelopment with php has been started
-//Module 4
+<?php
+
+//work with function
+
+function factorial($n){
+    $fac=1;
+    for($i=$n; $i>0; $i--){
+        $fac*=$i;
+    }
+    return $fac;
+}
 
 
+//optional perameter learning
 
+function daoyat($poramanik, $samsu){
+    return " poramniker bari theke $poramanik and samsu er bari theke $samsu daouayat paise";
+}
 
-echo "hello world" ;
-
+//sumation of 3 numbers
+function sumfixed(int $x,int $y,int $z):int {
+    return $x+$y+$z;
+}
+function sum(int ...$numbers):int {
+    $result=0;
+    foreach($numbers as $num){
+        $result+=$num;
+    }
+    return $result;
+}
 
 
 
--- php1.30.php
+++ php1.30.php
@@ -1,14 +1,8 @@
 <?php
-//work with function
+include_once "func.php";
 
-function factorial($n){
-    $fac=1;
-    for($i=$n; $i>0; $i--){
-        $fac*=$i;
-    }
-    return $fac;
-}
-echo factorial(6);
+echo PHP_EOL;
 
 
 
+echo sum(10,10,50,30);
