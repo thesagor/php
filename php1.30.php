c441b739ce3dd3298f913236ee53c1bc28045cbc
Sagor Ahmed <nilsagor004@gmail.com>
2024-01-28 11:00:53 +0600

    array reduce

--- php1.30.php
+++ php1.30.php
@@ -1,18 +1,18 @@
 <?php
 //learning array
 
-$students=[
-    'ashik',
-    'bacchu',
-    'cacchu',
-    'hayder',
-    'nain'
-];
-$n=count($students);
-for($i=$n-1; $i>=0; $i--){
-    echo strtoupper($students[$i]). "<br>";
-}
+//ARRAY REDUCE
 
+$number= [0,1,2,3,4,5,6,7];
+
+function suma($old,$new){
+    if($new%2==0) {
+        return $old + $new;
+    }
+    return $old;
+}
+$mainsum= array_reduce($number,'suma');
+echo $mainsum;
 
 
 
