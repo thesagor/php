95ed3ebacd075893dc5f8d81b397f0aa821d1956
Sagor Ahmed <nilsagor004@gmail.com>
2024-02-02 07:27:08 +0600

    range

--- php1.30.php
+++ php1.30.php
@@ -1,24 +1,6 @@
 <?php
 //learning array
-//shallow copy and deep copy
-
-$number=[
-    'a'=>100,
-    'b'=>500,
-    'z'=>250,
-    'd'=>300,
-    'e'=>350
-];
-$case=[
-    'a'=>'hellow',
-    'c'=>'Hai',
-    'b'=>'world'
-];
-asort($case, SORT_STRING | SORT_FLAG_CASE);
-asort($number);
-print_r($case);
-print_r($number);
-
-
-
-
+//
+foreach(range(0,20,2) as $evennumber){
+    echo $evennumber."\n";
+}
