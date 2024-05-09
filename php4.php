f6e9bb30137373437d8d84346750f8fcafda6425
Sagor Ahmed <nilsagor004@gmail.com>
2024-05-09 07:05:07 +0600

    using of substr and strrev function

--- php4.php
+++ php4.php
@@ -1,28 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ASCII</title>
-</head>
-<body>
+<?php
+    $string= "Hello world";
 
-    <!-- create a two column table -->
-    <table border="1">
-        <tr>
-            <td>ASCII</td>
-            <td>Character</td>
-        </tr>
-        <?php
-            for ($i=0; $i<128;$i++){
-                printf("<tr><td>%s</td><td>%s</td></tr>",$i,chr($i));
-            }
+    echo $string[3];
+    echo PHP_EOL;
+    echo strrev($string);
 
-        ?>
-
-    </table>
-
-</body>
-</html>
+?>
