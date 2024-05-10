3a9572f2a01c70088d4bfd53e78e508184fecbfe
Sagor Ahmed <nilsagor004@gmail.com>
2024-05-10 21:56:51 +0600

    string explode

--- php4.php
+++ php4.php
@@ -1,23 +1,15 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
 //data scrumbler project where has encode decode key generate button
-
 //for loop
 
-//foreach loop
+$string = "Hello World, We are people and we are man";
+$alada = explode(" ", $string);
 
 
-?>
+$preg = preg_split("/ |,/", $string);
+print_r($preg);
 
 
-</body>
-</html>
+
+
+
