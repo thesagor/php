879b487e3b821d76065a6db8f90cfca89fd8efb4
Sagor Ahmed <nilsagor004@gmail.com>
2024-05-19 21:14:29 +0600

    file_get_content

--- php4.php
+++ php4.php
@@ -2,12 +2,8 @@
 //data read
 
 $data = 'C:/Users/User/Desktop/php/inc/info.txt';
-$fo = fopen($data, "r+");
-$fgets = fgets($fo);
-
-echo "$fgets";
-fclose($fo);
-
+$fcontent = file_get_contents($data);
+echo $fcontent;
 
 
 
