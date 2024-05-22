dae1cc22f63d805db6e9de925d0083ab2d63c3ee
Sagor Ahmed <nilsagor004@gmail.com>
2024-05-22 06:05:08 +0600

    append mode

--- php4.php
+++ php4.php
@@ -1,9 +1,8 @@
 <?php
 //data read
 $data = 'C:/Users/User/Desktop/php/inc/file2.txt';
-$olddata = file_get_contents($data);
-$fo = fopen($data, "w");
-$existed = fwrite($fo, $olddata);
+
+$fo = fopen($data, "a");
 fwrite($fo, "Plito\n");
 fwrite($fo, "neptun\n");
 fwrite($fo, "navakump\n");
