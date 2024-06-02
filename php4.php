95c14c7f5baf5e20ff5eab71c084d4106850ec31
Sagor Ahmed <nilsagor004@gmail.com>
2024-06-03 05:49:16 +0600

    file_put_contents()

--- php4.php
+++ php4.php
@@ -1,14 +1,7 @@
 <?php
 //data read
 $data = 'C:/Users/User/Desktop/php/inc/info.txt';
-$fo = fopen($data, "a+");
-
-$fchek = fgets($fo);
-echo $fchek;
-rewind($fo);
-
-
-fwrite($fo, "extraday");
+file_put_contents($data, file_get_contents('inc/file2.txt'), FILE_APPEND | LOCK_EX);
 
 
 
