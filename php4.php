705573531f40abb0117c5d8c430262fe5a080258
Sagor Ahmed <nilsagor004@gmail.com>
2024-06-03 10:12:41 +0600

    array file insert

--- php4.php
+++ php4.php
@@ -1,7 +1,43 @@
 <?php
 //data read
-$data = 'C:/Users/User/Desktop/php/inc/info.txt';
-file_put_contents($data, file_get_contents('inc/file2.txt'), FILE_APPEND | LOCK_EX);
+$data = 'C:/Users/User/Desktop/php/inc/students.txt';
+
+
+$students = array(
+    array(
+        'name' => 'John Doe',
+        'age' => 20,
+        'course' => 'BSCS',
+        'year' => 3
+    ),
+    array(
+        'name' => 'Jane Adam',
+        'age' => 21,
+        'course' => 'BSIT',
+        'year' => 4
+    ),
+    array(
+        'name' => 'Juan Dela Cruz',
+        'age' => 22,
+        'course' => 'BSIS',
+        'year' => 2
+    ),
+    array(
+        'name' => 'Pedro Penduko',
+        'age' => 23,
+        'course' => 'BSHRM',
+        'year' => 1
+    )
+);
+
+$handle = fopen($data, 'w');
+foreach ($students as $student) {
+    //fputcsv($handle, $student);
+    fwrite($handle, $student['name'] . ', ' . $student['age'] . ',' . $student['course'] . ',' . $student['year'] . "\n");
+}
+fclose($handle);
+
+
 
 
 
