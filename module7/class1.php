7b71d8bb933ff9bb8c763aad4871399f15aca82e
Sagor Ahmed <nilsagor004@gmail.com>
2024-07-17 07:56:40 +0600

    loop over multidimentional associative array

--- module7/class1.php
+++ module7/class1.php
@@ -1,14 +1,29 @@
 <?php
-//Assosiative array
-
-$CEO = [
+//multidimentional associative array
+$microsoft = [
     'name' => "bill gates",
     'age' => 65,
     'mail' => "mail@bill.com",
     'phone' => "015884458"
 ];
-//loop over asssosiative array
 
-foreach ($CEO as $microsoft => $info) {
-    echo "$microsoft: $info \n";
-}
+$tesla = [
+    'name' => 'Elon mask',
+    'age' => 55,
+    'mail' => "mail@mask.com",
+    'phone' => "02258744"
+];
+
+$facebook = [
+    'name' => "juker barg",
+    'age' => 45,
+    'mail' => 'mail#juk.com',
+    'phone' => '01558447'
+];
+
+$CEO = [$microsoft, $tesla, $facebook];
+
+print_r($CEO[0]['name']);
+
+
+
