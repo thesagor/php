df9862c7533b6a9f6f09f177a69afeda74f73445
Sagor Ahmed <nilsagor004@gmail.com>
2024-07-21 09:45:02 +0600

    charge for over word

--- php4.php
+++ php4.php
@@ -1,60 +1,16 @@
 <?php
-//data read
-$data = 'C:/Users/User/Desktop/php/inc/students.txt';
+//string related issue
 
+$string = "I'm Sagor ahmed, we are lorem30 and try to work from home.Butt we have not  any internet connection";
 
-$students = array(
-    array(
-        'name' => 'John Doe',
-        'age' => 20,
-        'course' => 'BSCS',
-        'year' => 3
-    ),
-    array(
-        'name' => 'Jane Adam',
-        'age' => 21,
-        'course' => 'BSIT',
-        'year' => 4
-    ),
-    array(
-        'name' => 'Juan Dela Cruz',
-        'age' => 22,
-        'course' => 'BSIS',
-        'year' => 2
-    ),
-    array(
-        'name' => 'Pedro Penduko',
-        'age' => 23,
-        'course' => 'BSHRM',
-        'year' => 1
-    )
-);
+$lenth = strlen($string);
 
+$wordexplode = explode(" ", $string);
+$wcount = count($wordexplode);
+echo $wcount . "\n ";
+$charge = ($wcount - 5) * 5;
+echo $charge;
 
-function tastia($name, $age)
-{
-    echo " mY name is taslkia. How can i help you";
-    array(
-        'name' => "ahmed niaz",
-        'age' => 20,
-        'villa' => "chowbaria"
-    );
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-<ol>
-    <li></li>
-</ol>
-</body>
-
-</html>
+$charged = $charge / 5;
+echo PHP_EOL;
+echo $charged;
