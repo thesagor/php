ce471f6a29039b8937e5e1c7293e0225ad9729a8
Sagor Ahmed <nilsagor004@gmail.com>
2024-08-09 09:22:56 +0600

    OOP

---
+++ module8/part1.php
@@ -0,0 +1,19 @@
+<?php
+$echo = "ostad";
+function name()
+{
+    echo "Ostad is coming soon";
+}
+
+class math
+{
+    public $ver = "hope you are mine";
+
+    function nameis()
+    {
+        echo "echo is another";
+    }
+}
+
+$practise = new math();
+echo $practise->nameis();
