d15bcb9b9d71c866abde2c7261729a9c7565821d
Sagor Ahmed <nilsagor004@gmail.com>
2024-09-17 06:43:55 +0600

    laravel learning

---
+++ module9/practice/index.php
@@ -0,0 +1,31 @@
+<?php
+//working with json
+/*$arr = ['name' => 'sagor ahmed', 'age' => 30, 'email' => 'x@gm.com'];
+$jsonen = json_encode($arr);
+//echo $jsonen;
+$jsondecode = json_decode($jsonen);
+print_r($jsondecode);*/
+
+/*//json encode and decode in multidimentional array
+$jsonarray = [
+    [
+        'name' => "sagor ahmed",
+        'age' => 90,
+        'email' => 'nil@g.com'
+    ],
+    [
+        'name' => "hasnat ahmed",
+        'age' => 10,
+        'email' => ' nil@g.com'
+    ]
+];
+
+$jsonencode = json_encode($jsonarray);
+$jsonstring = '[{"name":"sagor ahmed","age":90,"email":"nil@g.com"},{"name":"hasnat ahmed","age":10,"email":" nil@g.com"}]';
+$jsondecode = json_decode($jsonstring, true);
+print_r($jsondecode);*/
+
+
+//session
+
+
---
+++ module9/practice/sessions/part1.php
@@ -0,0 +1,10 @@
+<?php
+
+
+session_start();
+$_SESSION['name'] = 'Sagor Ahmed';
+$_SESSION['email'] = "nilsagor004@gmail.com";
+$_SESSION['age'] = '30';
+
+
+
