2ff6852c4f48f94d7c71bb1cc6ed2061bf53f10f
Sagor Ahmed <nilsagor004@gmail.com>
2024-10-11 20:19:56 +0600

    throttle: added

--- module16/blade/routes/web.php
+++ module16/blade/routes/web.php
@@ -12,14 +12,6 @@ Route::get('/', function () {
 
 //Route::get('/test', [siteController::class, 'test'])->middleware(testMiddleware::class);
 
-/*// Group routes and apply middleware
-Route::middleware([testMiddleware::class])->group(function () {
-    Route::get('/test', [siteController::class, 'test']);
-    Route::get('/test1', [siteController::class, 'test1']);
-    Route::get('/test2', [siteController::class, 'test2']);
-});*/
-
-Route::get('/test3', [siteController::class, 'test3']);
-Route::get('/test', [siteController::class, 'test']);
-Route::get('/test1', [siteController::class, 'test1']);
-Route::get('/test2', [siteController::class, 'test2']);
+Route::middleware('throttle:2,1')->group(function () {
+    Route::get('/', [siteController::class, 'index']);
+});
