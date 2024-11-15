07501c82256a4ad28b93181e09dc4bc6e1f9a1f5
Sagor Ahmed <nilsagor004@gmail.com>
2024-11-15 21:27:52 +0600

    javascript last part

---
+++ module21/script.js
@@ -0,0 +1,25 @@
+/*
+function name(name) {
+    console.log(name);
+}
+name('module21');*/
+
+/*const name = function (x) {
+    console.log(`this is the ${x} square of ${x * x} `);
+}
+name(5);*/
+
+/*
+let name = (a, b) => {
+    console.log(`this is the sum of ${a + b}`);
+}
+name(5, 6);*/
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let squareofnumbers = numbers.map((x) => {
+    return x * x;
+})
+let fileter = numbers.filter(() => {
+})
+
+
